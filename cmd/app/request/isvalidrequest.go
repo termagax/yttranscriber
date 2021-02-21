baa257eebb50234feaa6a05b612a1da31b7acb5b
termagax <termagax@protonmail.com>
2021-02-21 05:41:04 -0800

    feat: got url,
    also used a janky way of checking if url is valid, so be nice testers :)
    
    updates issue #12

---
+++ cmd/app/request/isvalidrequest.go
@@ -0,0 +1,18 @@
+package request
+
+import "net/url"
+
+// LinkIsValid ...
+func LinkIsValid(uniresloc string) bool {
+	_, err := url.ParseRequestURI(uniresloc)
+	if err != nil {
+		return false
+	}
+
+	u, err := url.Parse(uniresloc)
+	if err != nil || u.Scheme == "" || u.Host == "" {
+		return false
+	}
+
+	return true
+}
