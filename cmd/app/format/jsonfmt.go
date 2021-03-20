f23d3999c2e7b4b7e9f7047e22f392159328dc13
termagax <termagax@protonmail.com>
2021-03-20 07:30:52 -0700

    feat: formatting json is hard
    very satisfying to get the json all properly formatted
    
    fixes issue #16

--- cmd/app/format/jsonfmt.go
+++ cmd/app/format/jsonfmt.go
@@ -1,6 +1,34 @@
 package format
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 // JSON ...
-func JSON(json string) string {
-	return "hello"
+func JSON(raw string) string {
+	var transcript string
+
+	var junkNav map[string]interface{}
+	if err := json.Unmarshal([]byte(raw), &junkNav); err != nil {
+		panic(err)
+	}
+
+	getEventsOutOfJunk := junkNav["events"].([]interface{})
+
+	for _, v := range getEventsOutOfJunk {
+		getSegsOutOfEvents := v.(map[string]interface{})
+		if getSegsOutOfEvents["segs"] != nil {
+			lol := getSegsOutOfEvents["segs"]
+			list := lol.([]interface{})
+			for _, i := range list {
+				netlock := i.(map[string]interface{})
+				s := fmt.Sprintf("%v", netlock["utf8"])
+				transcript += s
+			}
+		}
+	}
+
+	return transcript
+
 }
